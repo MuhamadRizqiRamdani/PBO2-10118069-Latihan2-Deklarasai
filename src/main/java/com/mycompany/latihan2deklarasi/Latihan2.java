1aaf4c3c73600b0c36255d1e49af151a0780a5a3
Muhamad Rizqi Ramdani <55569266+MuhamadRizqiRamdani@users.noreply.github.com>
2019-09-26 14:47:54 +0700

    Add files via upload

---
+++ src/main/java/com/mycompany/latihan2deklarasi/Latihan2.java
@@ -0,0 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.latihan2deklarasi;
+
+/**
+ *
+ * @author
+ * NAMA     : Muhamad Rizqi Ramdani
+ * KELAS    : PBO2
+ * NIM      : 10118069
+ * Deskripsi program : Program ini berisi program untuk menampilkan deklasarasi
+ */
+public class Latihan2 {
+    public static void main(String[] args) {
+        int nilaiInt;
+        final double PHI = 3.14; 
+        boolean nilaiLogika;
+        char nilaiKarakter;
+        
+        // Memberi nilai ke varibel
+        nilaiInt = 78;
+        nilaiLogika = false;
+        nilaiKarakter = 'D';
+        
+        //menampilkan hail
+        System.out.println();
+        System.out.println("Isi variabel nilai = " + nilaiInt);
+        System.out.println("Isi variabel PHI = " + PHI);
+        System.out.println("Isi variabel logika = " + nilaiLogika);
+        System.out.println("Isi variabel Karakter = " + nilaiKarakter);
+    }
+}
